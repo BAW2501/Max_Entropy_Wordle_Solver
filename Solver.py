6751668f501206a7f546622b935bdbaebde30478
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2023-02-02 19:45:24 +0100

    unrolled combination index for speed

--- Solver.py
+++ Solver.py
@@ -1,5 +1,6 @@
 import numpy as np
-import time
+from tqdm import tqdm
+
 
 
 def evaluate(_hyp, _ans):
@@ -13,22 +14,39 @@ def evaluate(_hyp, _ans):
             _tiles[i] = 1
     return _tiles
 
-
 def comb_index(_hyp, _ans):
     _ans = list(_ans)
     index = 0
-    for i in range(5):
-        if _hyp[i] == _ans[i]:
-            index += 2 * (3 ** i)
-            _ans[i] = '0'
-        elif _hyp[i] in _ans:
-            index += 1 * (3 ** i)
+    if _hyp[0] == _ans[0]:
+        index += 2 * (3 ** 0)
+        _ans[0] = '0'
+    elif _hyp[0] in _ans:
+        index += 1 * (3 ** 0)
+    if _hyp[1] == _ans[1]:
+        index += 2 * (3 ** 1)
+        _ans[1] = '0'
+    elif _hyp[1] in _ans:
+        index += 1 * (3 ** 1)
+    if _hyp[2] == _ans[2]:
+        index += 2 * (3 ** 2)
+        _ans[2] = '0'
+    elif _hyp[2] in _ans:
+        index += 1 * (3 ** 2)
+    if _hyp[3] == _ans[3]:
+        index += 2 * (3 ** 3)
+        _ans[3] = '0'
+    elif _hyp[3] in _ans:
+        index += 1 * (3 ** 3)
+    if _hyp[4] == _ans[4]:
+        index += 2 * (3 ** 4)
+    elif _hyp[4] in _ans:
+        index += 1 * (3 ** 4)
     return index
 
 
 def entropies():
     entropies_calc = np.zeros(len(all_words), dtype=np.float64)
-    for i, _hyp in enumerate(all_words):
+    for i, _hyp in tqdm(enumerate(all_words),total=len(all_words)):
         combs = [comb_index(_hyp, _ans) for _ans in hidden_words]
         proba = np.bincount(combs, minlength=3 ** 5) / len(hidden_words)
         entropies_calc[i] = -np.sum(proba * np.log2(proba, out=np.zeros_like(proba), where=(proba != 0)))
@@ -59,10 +77,7 @@ if __name__ == "__main__":
 
     for cnt in range(6):
         print(f'Round {cnt + 1}:')
-        start = time.time()
-        hyp = guess()
-        end = time.time()
-        print(f'Execution time: {end - start:.2f}s')
+        hyp = guess() #if cnt != 0 else 'soare'
         print(f'Guess: {hyp} evaluated as {evaluate(hyp, ans)}')
 
         if all(evaluate(hyp, ans) == 2):
